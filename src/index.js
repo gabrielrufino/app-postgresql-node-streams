3c003fd2f195d27c08bacfe4e7b2fd16c58ff13b
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-12 22:40:39 -0300

    Using try/catch

--- src/index.js
+++ src/index.js
@@ -6,36 +6,41 @@ import { promisify } from 'util'
 import { pipeline } from 'stream'
 import { createWriteStream } from 'fs'
 
-const {
-  DATABASE_URL,
-  DATABASE_NAME,
-  TABLE_NAME
-} = process.env
-
-const client = new pg.Client({
-  connectionString: `${DATABASE_URL}/${DATABASE_NAME}`
-})
-
-await client.connect()
-
-const query = new QueryStream(`SELECT * FROM public.${TABLE_NAME}`)
-const stream = client.query(query)
-
-await promisify(pipeline)(
-  stream,
-  async function *(stream) {
-    yield ['ID', 'Type', 'Breed', 'Owner name', 'Owner email', 'Owner phone', 'Register date']
-
-    for await (const chunk of stream) {
-      yield Object.values(chunk)
-    }
-  },
-  async function *(lines) {
-    for await (const line of lines) {
-      yield line.join(',') + '\n'
-    }
-  },
-  createWriteStream('dataset.csv')
-)
-
-await client.end()
+try {
+  const {
+    DATABASE_URL,
+    DATABASE_NAME,
+    TABLE_NAME
+  } = process.env
+  
+  const client = new pg.Client({
+    connectionString: `${DATABASE_URL}/${DATABASE_NAME}`
+  })
+  
+  await client.connect()
+  
+  const query = new QueryStream(`SELECT * FROM public.${TABLE_NAME}`)
+  const stream = client.query(query)
+  
+  await promisify(pipeline)(
+    stream,
+    async function *(stream) {
+      yield ['ID', 'Type', 'Breed', 'Owner name', 'Owner email', 'Owner phone', 'Register date']
+  
+      for await (const chunk of stream) {
+        yield Object.values(chunk)
+      }
+    },
+    async function *(lines) {
+      for await (const line of lines) {
+        yield line.join(',') + '\n'
+      }
+    },
+    createWriteStream('dataset.csv')
+  )
+  
+  await client.end()
+} catch (error) {
+  console.error(error.message)
+  process.exit(1)
+}
--- src/seed.js
+++ src/seed.js
@@ -5,68 +5,73 @@ import faker from 'faker'
 import { promisify } from 'util'
 import { pipeline } from 'stream'
 
-const {
-  DATABASE_URL,
-  DATABASE_NAME,
-  TABLE_NAME,
-  TABLE_SIZE
-} = process.env
-
-let client
-
-client = new pg.Client({
-  connectionString: `${DATABASE_URL}/postgres`
-})
-
-await client.connect()
-await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}`)
-await client.query(`CREATE DATABASE ${DATABASE_NAME}`)
-await client.end()
-
-client = new pg.Client({
-  connectionString: `${DATABASE_URL}/${DATABASE_NAME}`
-})
-
-await client.connect()
-await client.query(`
-  CREATE TABLE public.${TABLE_NAME} (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    breed VARCHAR(255) NOT NULL,
-    owner_name VARCHAR(255) NOT NULL,
-    owner_email VARCHAR(255) NOT NULL,
-    owner_phone VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  )
-`)
-
-await promisify(pipeline)(
-  async function *() {
-    for (const _ of Array(Number(TABLE_SIZE)).fill(null)) {
-      const type = faker.animal.type()
-      const data = [type, faker.animal[type](), faker.name.findName(), faker.internet.email(), faker.phone.phoneNumber()]
-
-      yield data
-    }
-  },
-  async function *(stream) {
-    for await (const data of stream) {
-      const query = {
-        text: `
-          INSERT INTO public.${TABLE_NAME} (type, breed, owner_name, owner_email, owner_phone)
-          VALUES ($1, $2, $3, $4, $5)
-        `,
-        values: [...data]
+try {
+  const {
+    DATABASE_URL,
+    DATABASE_NAME,
+    TABLE_NAME,
+    TABLE_SIZE
+  } = process.env
+  
+  let client
+  
+  client = new pg.Client({
+    connectionString: `${DATABASE_URL}/postgres`
+  })
+  
+  await client.connect()
+  await client.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}`)
+  await client.query(`CREATE DATABASE ${DATABASE_NAME}`)
+  await client.end()
+  
+  client = new pg.Client({
+    connectionString: `${DATABASE_URL}/${DATABASE_NAME}`
+  })
+  
+  await client.connect()
+  await client.query(`
+    CREATE TABLE public.${TABLE_NAME} (
+      id SERIAL PRIMARY KEY,
+      type VARCHAR(255) NOT NULL,
+      breed VARCHAR(255) NOT NULL,
+      owner_name VARCHAR(255) NOT NULL,
+      owner_email VARCHAR(255) NOT NULL,
+      owner_phone VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `)
+  
+  await promisify(pipeline)(
+    async function *() {
+      for (const _ of Array(Number(TABLE_SIZE)).fill(null)) {
+        const type = faker.animal.type()
+        const data = [type, faker.animal[type](), faker.name.findName(), faker.internet.email(), faker.phone.phoneNumber()]
+  
+        yield data
       }
-
-      yield query
+    },
+    async function *(stream) {
+      for await (const data of stream) {
+        const query = {
+          text: `
+            INSERT INTO public.${TABLE_NAME} (type, breed, owner_name, owner_email, owner_phone)
+            VALUES ($1, $2, $3, $4, $5)
+          `,
+          values: [...data]
+        }
+  
+        yield query
+      }
+    },
+    async function *(stream) {
+      for await (const query of stream) {
+        await client.query(query)
+      }
     }
-  },
-  async function *(stream) {
-    for await (const query of stream) {
-      await client.query(query)
-    }
-  }
-)
-
-await client.end()
+  )
+  
+  await client.end()
+} catch (error) {
+  console.error(error.message)
+  process.exit()
+}
